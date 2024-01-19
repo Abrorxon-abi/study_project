9b379828d415905e522f25da266901c6c2b1ed72
Abrorxon-abi <ppvbuyppv@gmail.com>
2024-01-19 17:22:42 +0500

    some changes

--- frontend/src/components/Products/Item.jsx
+++ frontend/src/components/Products/Item.jsx
@@ -3,21 +3,18 @@ import { BASE_URL } from '../../conf/store.js'
 import ZoomIn from "../../assets/icons/search-vector.png"
 import Wishlist from "../../assets/icons/wishlist-vector.png"
 import Cart from "../../assets/icons/card-vector.png"
-import { axiosCall } from '../../conf/axios.js'
+import { authorizedAxiosCall } from '../../conf/axios.js'
 import { toast } from 'react-toastify'
+import { Link } from 'react-router-dom'
 
 function Item(props) {
     const DISCOUNT = 0.35
     const discountPrice = props.price - (props.price * DISCOUNT)
-    // delete_furniture
 
     async function addToWishlist(e) {
         e.preventDefault()
         const data = { furniture_id: props.itemID }
-        const response = await axiosCall(`api/furniture/wishlist/`, data, 
-        {
-            "Authorization": "Bearer " + localStorage.getItem("access_token"),
-        }, "POST")
+        const response = await authorizedAxiosCall(`api/furniture/wishlist/`, data, "POST")
         console.log(response)
         toast.success("Successfully added into wishlist", {toastId: 14})
     }
@@ -25,9 +22,16 @@ function Item(props) {
     return (
         <div className="product-item-wrapper">
             <div className="hover-info">
-                <span><img src={Cart} alt="Cart" /></span>
-                <span onClick={addToWishlist} ><img src={Wishlist} alt="Wishlist" /></span>
-                <span><img src={ZoomIn} alt="ZoomIn" /></span>
+
+                <span 
+                    className="add-to-wishlist-btn" 
+                    onClick={addToWishlist} >
+                        <img src={Wishlist} alt="Wishlist" width={22} height={22} />
+                </span>
+
+                <Link to={`/furniture-details/${props.itemID}`}>
+                    <span><img className="view-item" src={ZoomIn} alt="ZoomIn" /></span>
+                </Link>
             </div>
             <img
                 src={BASE_URL + props.image}
